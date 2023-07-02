5867a3a84ce04c01073bb44943bc52e204257fec
CalumRaine <calumiscoding@gmail.com>
2023-07-02 20:30:55 +0100

    Ignore Meta key

--- caret.js
+++ caret.js
@@ -1,12 +1,21 @@
+/*
+	You have to separate document content responsibilities from document display responsibilities.
+	The document caret interfaces between both.  
+		-- User presses left arrow?  Means move caret left in document buffer.
+		-- User presses up arrow?  Means move caret upward in document display.
+	A new line in the document content forces a new line.  But the display can also wrap lines as it needs to.
+ */ 
+
 class Caret {
 	contentCaret = new ContentCaret();
+	displayCaret = null; // not yet implemented
 	
 	constructor(){
 		document.addEventListener("keydown", (event) => this.handleKey(event));
 	}
 
 	handleKey(event){
-		const specialKeys = ["Ctrl", "Alt", "Shift"];
+		const specialKeys = ["Ctrl", "Alt", "Shift", "Meta"];
 		if (event.key.includes("Arrow")){
 			return this.handleArrow(event);
 		}
