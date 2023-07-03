4238c72fd61ee391c634f92557009be0d9236ad6
Calum RAINE <calum.raine@vinci-construction.com>
2023-07-03 15:36:31 +0100

    Looks like this wasn't edited properly

--- documentContent.js
+++ documentContent.js
@@ -1,61 +1,70 @@
 class Content {
-	lines = [];
+	pages = [];
 
-	constructor(lines){
-		this.lines = lines == null ? [new Line()] : lines;
+	constructor(pages){
+		this.pages = pages == null ? [new Page()] : pages;
 	}
 
-	adopt(lines){
-		this.lines = lines;
-		return line;
+	adopt(pages){
+		this.pages = pages;
+		return true;
+	}
+
+	get Characters(){
+		return this.pages.flatMap(p => p.Characters);
 	}
 
 	get Text(){
-		return this.lines.map(l => l.Text).join("");
+		return this.pages.map(p => p.Text).join("");
 	}
 
 	get WordCount(){
-		return this.lines.reduce((sum, line) => sum + line.WordCount, 0);
+		return this.pages.reduce((sum, page) => sum + page.WordCount, 0);
 	}
 
 	get LastIndex(){
-		return this.lines.length - 1;
+		return this.pages.length - 1;
 	}
 
 	grabCaret(caret, toEnd){
 		caret.page = this;
-		return this.lines[toEnd ? this.LastIndex : 0].grabCaret(caret, toEnd);
+		return this.pages[toEnd ? this.LastIndex : 0].grabCaret(caret, toEnd);
 	}
 
 	getCaretIndex(caret){
-		return this.lines.findIndex(l => l == caret.line);
+		return this.pages.findIndex(p => p == caret.page);
 	}
 
 	left(caret){
 		let index = this.getCaretIndex(caret);
-		return index == 0 ? false : this.lines[index - 1].grabCaret(caret, true);
+		return index == 0 ? false : this.pages[index - 1].grabCaret(caret, true);
 	}
 
 	right(caret){
 		let index = this.getCaretIndex(caret);
-		return index == this.LastIndex ? false : this.lines[index + 1].grabCaret(caret, false);
+		return index == this.LastIndex ? false : this.pages[index + 1].grabCaret(caret, false);
 	}
 
 	split(caret){
-		this.lineBreak(caret);
+		this.pageBreak(caret);
 		let index = this.getCaretIndex(caret);
 		let toExtract = this.LastIndex - index;
 		let extractedLines = this.lines.splice(index, toExtract);
-		let newPage = new Page();
-		newPage.adopt(extractedLines);
+		let newPage = new Page(extractedLines);
 		return newPage;
 	}
 
-	lineBreak(caret){
+	pageBreak(caret){
 		let index = this.getCaretIndex(caret);
-		let brokenLine = this.lines[index].split(caret);
-		this.lines.splice(index, 0, brokenLine);
-		brokenLine.grabCaret(caret, false);
+		let page = this.pages[index];
+		let brokenPage = page.split(caret);
+		this.insertPageAfter(brokenPage, index);
+		return true;
+	}
+
+	insertPageAfter(page, index){
+		this.lines.splice(index + 1, 0, page);
+		page.grabCaret(caret, false);
 		return true;
 	}
 }
