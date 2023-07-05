6edffc98c6527d5a91a3c00e47acf946f33ddc90
CalumRaine <calumiscoding@gmail.com>
2023-07-05 16:00:34 +0100

    Restrict so only WRAPPED lines can't begin with whitespace

--- line.js
+++ line.js
@@ -26,7 +26,7 @@ class Line {
 					// Line must wrap.  No more characters can fit on line.
 					return wrappedWords.length == 0 ? null : wrappedWords;
 				}
-				else if (wrappedWords.length > 0 || wordToParse.IsTrueWord) {	// Don't start a line with whitespace
+				else if (this.parseCursor == 0 || wrappedWords.length > 0 || wordToParse.IsTrueWord) {	// Don't start a wrapped line with whitespace
 					let wrappedWord = new WrappedWord(wrappedCharacters);
 					maxWidth -= wrappedWord.Width;
 					wrappedWords.push(wrappedWord);
